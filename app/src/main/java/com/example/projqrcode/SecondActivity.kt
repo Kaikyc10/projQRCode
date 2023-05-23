da454fe216713dbc277cbecc519235741653bd1d
Kaikyc10 <kaikycostatereza@gmail.com>
2023-05-23 17:48:41 -0300

    PROJETO ATUALIZADO

--- app/src/main/java/com/example/projqrcode/SecondActivity.kt
+++ app/src/main/java/com/example/projqrcode/SecondActivity.kt
@@ -1,14 +1,32 @@
 package com.example.projqrcode
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import com.example.projqrcode.SubmissionData
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+
+    private fun generateQRCodeBitmap(message: String): Bitmap {
+        val qrCodeWriter = QRCodeWriter()
+        val bitMatrix = qrCodeWriter.encode(message, BarcodeFormat.QR_CODE, 500, 500)
+        val width = bitMatrix.width
+        val height = bitMatrix.height
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+            }
+        }
+        return bitmap
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +34,19 @@ class SecondActivity : AppCompatActivity() {
 
         // Obter os dados enviados da MainActivity
         val submissionData = intent.getParcelableExtra<SubmissionData>("submissionData")
+        if (submissionData != null) {
+            val ra = submissionData.ra
+            val raTextView: TextView = findViewById(R.id.raTextView)
+            raTextView.text = ra
+
+            val qrCodeMessage = submissionData.qrCodeMessage
+            val qrCodeBitmap = generateQRCodeBitmap(qrCodeMessage)
+            val qrCodeImageView: ImageView = findViewById(R.id.qrCodeImageView)
+            qrCodeImageView.setImageBitmap(qrCodeBitmap)
+
+        }
+
+
 
 // Exibir os dados na segunda tela
         val qrCodeImageView = findViewById<ImageView>(R.id.qrCodeImageView)
@@ -24,13 +55,13 @@ class SecondActivity : AppCompatActivity() {
         val locationTextView = findViewById<TextView>(R.id.locationTextView)
 
 // Exibir a imagem do QR Code lido
-        qrCodeImageView.setImageBitmap(submissionData?.qrCodeBitmap)
+       // qrCodeImageView.setImageBitmap(submissionData?.qrCodeBitmap)
 
 // Exibir a foto tirada pelo usuário
         photoImageView.setImageBitmap(submissionData?.photoBitmap)
 
 // Exibir o RA digitado
-        raTextView.text = submissionData?.ra
+        //raTextView.text = submissionData?.ra
 
 // Exibir a localização do aluno
         val location = submissionData?.location
@@ -46,8 +77,8 @@ class SecondActivity : AppCompatActivity() {
         //val longitude = intent.getDoubleExtra("longitude", 0.0)
 
         qrCodeImageView.setImageURI(Uri.parse(qrCode)) // Exibe o QR Code
-        fotoImageView.setImageURI(Uri.parse(fotoTirada)) // Exibe a foto tirada
-        raTextView.text = ra // Exibe o RA digitado
-        localizacaoTextView.text = "Latitude: $latitude, Longitude: $longitude" // Exibe a localização
+        //fotoImageView.setImageURI(Uri.parse(fotoTirada)) // Exibe a foto tirada
+        //raTextView.text = ra // Exibe o RA digitado
+       // localizacaoTextView.text = "Latitude: $latitude, Longitude: $longitude" // Exibe a localização
     }
 }
